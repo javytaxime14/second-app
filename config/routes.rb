d709d58ab8f637ab1817152c30550f4153d57b93
Javiera Hidalgo <javiera_56@hotmail.com>
2021-06-19 01:50:56 -0400

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/login'
+  get 'pages/index'
+  root "pages#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "pages/test"
   get  "users/login"
   get  "pages/about_us"
-  root "pages#index"
 end
